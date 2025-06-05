e1da71e71e0cdc8dfca54708440c0628e7097210
mynameisntluke <98903989+mynameisntluke@users.noreply.github.com>
2025-06-05 19:04:52 +0100

    Add Day One by upload

---
+++ Project1 Band Name Generator/NameGenerator.py
@@ -0,0 +1,35 @@
+# Day One Working with Variables
+
+
+# Ask user three questions
+print("Hello You!")
+input("Press any key to begin creating your new band name!")
+print("Please limit your answers to one or two words each")
+two = input("What is your favorite sweet treat?\n")
+three = input("Why are you here?\n")
+one = input("How did you find these questions?\n")
+
+# Return Band Name
+input("Okay I'm working on your new name, press any key when you're ready")
+print("Ta-Da!! Here is your new band name!")
+print("========================")
+print(one + " " + two + " " + three + ".")
+print("========================")
+
+
+
+
+
+#################### OLD STUFF ####################
+
+# questions_arr_1 = [
+#     "What town were you born in?",
+#     "What is your favorite animal?",
+#     "What is your favorite sweet treat?"
+# ]
+#
+# questions_arr_2 = [
+#     "What's the weather like?",
+#     "How are you feeling?",
+#     "Why are you here?"
+# ]
---
+++ Project1 Band Name Generator/lengthy.py
@@ -0,0 +1,3 @@
+num = input("Gimme a number\n")
+len_num = len(num)
+print("That number has " + str(len_num) + " letters yo.")
