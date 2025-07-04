31773003cafb0729807f03c526a952a5375ac49a
mynameisntluke <98903989+mynameisntluke@users.noreply.github.com>
2025-07-04 14:15:39 +0100

    Update main.py

--- JapanADay/main.py
+++ JapanADay/main.py
@@ -1,9 +1,22 @@
-from japan import JapanApp
+from japan_app import JapanApp
+
 
 app = JapanApp()
-print(
-    app.get_current_word_jp()
-)
-print(
-    app.get_current_word_eng()
-)
+
+#one round
+guess = " "
+while guess != "exit":
+
+    guess = input(f"{app.get_current_word_eng()}:")
+    correct_answer = app.get_current_word_jp()
+
+    if guess == correct_answer:
+	    print("correct")
+	    app.delete_current_word()
+	    app.set_random_eng_word()
+	   
+    elif guess == "exit":
+    	print("goodbye")
+    
+    else:
+    	print(f"The correcr answer was:{correct_answer}")
