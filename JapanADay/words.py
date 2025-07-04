19a0564cda8464da457b3bc359470e2f928f31d4
mynameisntluke <98903989+mynameisntluke@users.noreply.github.com>
2025-07-04 14:17:17 +0100

    Update words.py
    remove placeholder text and add example option

--- JapanADay/words.py
+++ JapanADay/words.py
@@ -1,5 +1,15 @@
 words = {
-    "English": "Japan",
-    "Question": "Answer",
-    "Another word to guess": "The Answer"
+    "Add": { 
+    	"Japanese": ["たす"], 
+    	"example":" "
+    }, 
+    "Minus": {
+    	"Japanese": ["ひく"],
+    	"example": " "
+    }, 
+    "Multiply": {
+    	"Japanese": ["かける"],
+    	"example": "五[____]四は二十です "
+    }
+    
 }
